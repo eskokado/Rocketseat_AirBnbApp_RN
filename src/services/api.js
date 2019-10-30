accbf04f269a946a9ebb060f4d62c8b680197006
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-30 09:23:57 -0300

    Configurando o host para acessar api através de celular

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 ** Simulador IOS:           http://localhost:3333/
 */
 const api =  axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://192.168.15.17:3333'
 });
 
 export default api;
